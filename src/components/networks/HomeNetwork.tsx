815a53f4d55e496a4eac29b68155ef1e7afc13ce
8dong <hyde981114@gmail.com>
2023-11-05 17:21:18 +0900

    feat: useQuery 및 useQuries 훅 추가 (#5)

--- src/components/networks/HomeNetwork.tsx
+++ src/components/networks/HomeNetwork.tsx
@@ -1,13 +1,18 @@
 import { FC, ReactNode } from 'react';
 
 import QueryBoundary from '@/shared/networks/QueryBoundary';
+import HomeContainer from '../containers/HomeContainer';
 
 interface IProps {
   children: ReactNode;
 }
 
 const HomeNetwork: FC<IProps> = ({ children }) => {
-  return <QueryBoundary>{children}</QueryBoundary>;
+  return (
+    <QueryBoundary>
+      <HomeContainer />
+    </QueryBoundary>
+  );
 };
 
 export default HomeNetwork;
